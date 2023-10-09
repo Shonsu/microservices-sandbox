569a8890c1818696b852a54888c72ee4b55069da
Shonsu <shonsu82@gmail.com>
2023-10-09 21:30:20 +0200

    add Micrometer and Zipkin

--- currency-conversion-service/src/main/java/pl/shonsu/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/pl/shonsu/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,16 +1,20 @@
 package pl.shonsu.microservices.currencyconversionservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
-import pl.shonsu.microservices.currencyconversionservice.feign.CurrencyExchangeProxy;
 import pl.shonsu.microservices.currencyconversionservice.controller.dto.CurrencyConversion;
+import pl.shonsu.microservices.currencyconversionservice.feign.CurrencyExchangeProxy;
 
 import java.math.BigDecimal;
 
 @RestController
 public class CurrencyConversionController {
+    private final static Logger logger = LoggerFactory.getLogger(CurrencyConversionController.class);
+
     private final WebClient webClient;
 
     private final CurrencyExchangeProxy proxy;
@@ -24,19 +28,21 @@ public class CurrencyConversionController {
     public CurrencyConversion calculateCurrencyConversion(@PathVariable String from,
                                                           @PathVariable String to,
                                                           @PathVariable BigDecimal quantity) {
-        CurrencyConversion currencyConversion = webClient.get().uri(uriBuilder -> uriBuilder.path("/currency-exchange/from/{from}/to/{to}")
+        logger.info("calculateCurrencyConversion called with {} {}", from, to);
+        CurrencyConversion currencyConversion = webClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/currency-exchange/from/{from}/to/{to}")
                 .build(from, to)).retrieve().bodyToMono(CurrencyConversion.class).block();
         return new CurrencyConversion(currencyConversion.id(), from, to, quantity,
                 currencyConversion.conversionMultiple(),
                 quantity.multiply(currencyConversion.conversionMultiple()),
-                currencyConversion.environment()  + " " + "WebClient");
+                currencyConversion.environment() + " " + "WebClient");
     }
 
     @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from,
-                                                          @PathVariable String to,
-                                                          @PathVariable BigDecimal quantity) {
-
+                                                               @PathVariable String to,
+                                                               @PathVariable BigDecimal quantity) {
+        logger.info("calculateCurrencyConversionFeign called with {} {}", from, to);
         CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
 
         return new CurrencyConversion(currencyConversion.id(), from, to, quantity,
